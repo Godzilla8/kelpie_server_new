26249b37439a6a5dbf599063ca9d3a1c8736373e
Godlove Chinenye Godwin <chinenye.godlove@gmail.com>
2024-12-11 09:38:57 +0100

    updated token signing

--- utils/createJwtToken.js
+++ utils/createJwtToken.js
@@ -1,12 +1,13 @@
+import "dotenv/config";
 import jwt from "jsonwebtoken";
 
 export const createJwtToken = (args) => {
-  const token = jwt.sign({ ...args }, process.env.JWT_SECRET, { expiresIn: "1h" });
+  const token = jwt.sign(args, process.env.JWT_SECRET, { expiresIn: "1h" });
   return token;
 };
 
 export const cookieOptions = {
   maxAge: 3600000,
-  // secure: true,
+  secure: true,
   httpOnly: true,
 };
