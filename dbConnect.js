31fd84aa24ac31ab1014753113373b94a667b28a
Godlove Chinenye Godwin <chinenye.godlove@gmail.com>
2024-12-22 19:07:44 +0100

    added mongoose connect options

--- dbConnect.js
+++ dbConnect.js
@@ -4,7 +4,10 @@ import mongoose from "mongoose";
 const connectDatabase = async () => {
   try {
     if (process.env.NODE_ENV === "production") {
-      await mongoose.connect(process.env.MONGODB_URI);
+      await mongoose.connect(process.env.MONGODB_URI, {
+        serverSelectionTimeoutMS: 30000, // Set timeout to 30 seconds
+        connectTimeoutMS: 30000, // Optional: Timeout for initial connection
+      });
     }
     if (process.env.NODE_ENV === "development") {
       await mongoose.connect(process.env.LOCAL_MONGO_URI);
